30977fa7433a28f80c771e3fd345f3d4a86439b9
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 16:58:54 +0530

    Added a line

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -5,6 +5,7 @@ package org.ravi.helloworld;
 */
 public class Hello {
 	private String user = "Bhushan";
+
 	public String displayHelloMessage(){
 		String message = "hi";
 		message += "there, I am "+ user;
