707521870cb4f22e8e4273591088cf8dffebdc1e
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 11:46:48 +0530

    Updated message

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -1,9 +1,10 @@
 package org.ravi.helloworld;
 
 public class Hello {
-	
+	private String user = "Bhushan";
 	public String displayHelloMessage(){
 		String message = "hi";
+		message += "there, I am "+ user;
 		return message;
 	}
 
